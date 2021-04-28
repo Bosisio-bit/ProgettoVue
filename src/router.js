30fc2f62af133b148aee717be3e68e5d27944236
Matteo Fezzardi <matteo.fezzardi@marconi.loc>
2021-04-28 10:20:54 +0200

    add

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+	import Router from 'vue-router'
+
+    import MyCal from './views/MyCal.vue'
+    import MyPin from './views/PinBoard.vue'
+    import MyToDo from './views/ToDo.vue'
+
+	Vue.use(Router)
+	export default new Router({
+	  routes: [
+		{
+		  path: '/calendar',
+		  name: 'Calendar',
+		  component: MyCal
+		},
+		{
+		  path: '/pinboard',
+		  name: 'Pinboard',
+		  component: MyPin
+		},
+        {
+            path: '/todo',
+            name: 'Todo',
+            component: MyToDo
+          }
+	  ]
+	})
